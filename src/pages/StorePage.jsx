f7fc9c15dd7ca06d1d26fa4fa47261bcbb763a15
Rohan kumar <rk220217@gmail.com>
2022-05-11 12:49:46 +0530

    filter component  completed

--- src/pages/StorePage.jsx
+++ src/pages/StorePage.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React from "react";
 
+import { ReactComponent as CartLogo } from "../assets/icons/cart.svg";
+import { ReactComponent as SearchLogo } from "../assets/icons/search.svg";
+import FilterComponent from "../components/FilterComponent";
+
+import classes from "./StorePage.module.css";
 
 function StorePage() {
-    return ( <p>This is StorePage</p> );
+  return (
+    <div className={classes.container}>
+      {/* Filter Component */}
+      <FilterComponent />
+
+      {/* Main Shop Component */}
+      <div className={[classes.shopContainer, classes.card].join(" ")}>
+        <div className={classes.headingContainer}>
+          <h1>Shoes</h1>
+          <div className={classes.headingButton}>
+            <SearchLogo />
+            <button>Sort By</button>
+          </div>
+        </div>
+      </div>
+
+      {/* Cart Component */}
+      <div className={[classes.cartContainer, classes.card].join(" ")}>
+        <div className={classes.headingContainer}>
+          <h1>Cart</h1>
+          <CartLogo />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default StorePage;
